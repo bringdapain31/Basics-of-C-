8889368152067c9199200e3471fe49496e1b8146
Preston LaMarca <bane3154@gmail.com>
2019-08-28 11:03:17 -0700

    Add files via upload

---
+++ Test_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Data Types */
+
+            /* string phrase = "Preston is awesome"; /*Stores Objects */
+            /* char grade = 'A'; /*one character */
+            /* int age = 30; /*integer stores numbers */
+            /* double gpa = 3.2; /*stores decimal numbers such as float and decimal does, but decimal is more accurate for as example dealing with money */
+            /* bool isMale = true; /*Stores true or false values */
+
+            /* Working with Numbers */
+
+            /* int num = 6;
+            num++;
+            Console.WriteLine(num); increased the number 6 by one to make the console print 7 */
+
+            /* Methods - block of code performing a specific form of task or function */
+
+            /* Console.WriteLine(Math.Abs(-40));  Returns the absolute value which is 40 */
+            /* Console.WriteLine( Math.Pow(3,2) );  Pow is equal to the power of this returns 9 since 3 * 3 (power of 2) is 9 */
+            /* Console.WriteLine( Math.Sqrt(36) );  Gives the square root of 36 which comes to 6 */
+            /* Console.WriteLine( Math.Max(4, 90) );  Returns the largest number "90" */
+            /* Console.WriteLine( Math.Round(4.6) );  Rounds 4.6 up to 5 */
+
+
+            /*Working with strings */
+
+            /* string name = "Preston" + " is Superman!";
+            Console.WriteLine(name.ToUpper()); /*Returns Preston is Superman! in capital letters */
+
+            /*.Contains returns a true or false Value */
+
+            /*string phrase = "Superman";
+            Console.WriteLine(phrase.Contains("Superman")); Returns true cause superman is equal to string phrase */
+
+            /*[] prints the second index in the string word = Coding Dojo */
+
+            /* string word="Coding Dojo";
+             Console.WriteLine( word[2]); */
+
+
+
+            /*variables contain different type of data */
+            /*below concatinates a string and integer */
+
+            /*  string characterName = "Aiden";
+             int characterAge;
+             characterAge = 25;
+
+             Console.WriteLine("There once was a man named " + characterName);
+             Console.WriteLine("He was " + characterAge + " years old");
+             Console.WriteLine("He really liked the name baby Thor");
+             Console.WriteLine("He really didnt like the name " + characterName); */
+        }
+    }
+}
